3579a52e8ae6c540bcdc0830ef0c1fbe739100d0
oscarpetersson <oscar@annotell.com>
2019-01-01 19:25:27 +0100

    add temp sensor

---
+++ tempsensor/temp.py
@@ -0,0 +1,29 @@
+from gpiozero import CPUTemperature
+from time import sleep, strftime, time
+import matplotlib.pyplot as plt
+
+cpu = CPUTemperature()
+
+plt.ion()
+x = []
+y = []
+
+
+def write_temp(temp):
+	with open("/home/pi/code/tempsensor/cpu_temp.csv", "a") as log:
+		log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp)))	
+		sleep(1)
+
+def graph(temp):
+	y.append(temp)
+	x.append(time())
+	plt.clf()
+	plt.scatter(x,y)
+	plt.plot(x,y)
+	plt.draw()
+
+while True:
+	temp = 	cpu.temperature
+	write_temp(temp)
+#	graph(temp)
+	sleep(1)
